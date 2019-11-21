26563af6d25fe5e9c89c687fdc2dff7dbd1bffd8
Daniel Martin <daniel@danieldevelops.com>
2019-11-21 01:54:03 -0500

    added new methods for being able to intercept onError

---
+++ ElmahCore.Demo/Models/LoginPage.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElmahCore.Demo.Models
+{
+    public class LoginPage
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
