4ea3325c6ba7452b5ed6cb7e4f54014c6460c19b
Андрей Найченко <naychenko@outlook.com>
2020-12-13 15:00:07 +0300

    NetCore 5 demo

---
+++ ElmahCore.DemoCore5/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ElmahCore.DemoCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElmahCore.DemoCore5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            throw new Exception("Test");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
