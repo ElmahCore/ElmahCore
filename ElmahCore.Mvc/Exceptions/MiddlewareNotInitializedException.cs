eb8524a2fcbc18b5a8f383e1362bd3ab1ab6d2cc
Jason Finch <jason.finch@gmail.com>
2021-09-14 08:40:20 +1000

    fix (naming): Fix name of Exception

--- ElmahCore.Mvc/Exceptions/MiddlewareNotInitializedException.cs
+++ ElmahCore.Mvc/Exceptions/MiddlewareNotInitializedException.cs
@@ -2,11 +2,19 @@
 
 namespace ElmahCore.Mvc.Exceptions
 {
+    /// <inheritdoc />
     public class MiddlewareNotInitializedException : Exception
     {
+        // ReSharper disable once UnusedMember.Global
+        public MiddlewareNotInitializedException()
+        {
+        }
+
         public MiddlewareNotInitializedException(string message) : base(message)
         {
         }
+
+        // ReSharper disable once UnusedMember.Global
 
         public MiddlewareNotInitializedException(string message, Exception innerException) : base(message, innerException)
         {
