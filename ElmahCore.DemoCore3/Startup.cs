25688af3ff773c6dfa7b42e82252c3a5a38c83db
Zolaikha Hammad <70544823+zolaikhahammad@users.noreply.github.com>
2022-08-14 01:22:01 +0500

    Revert "Fixed Open Issues"
    This reverts commit 0a1053da892285db26512fb165ca6544cc40cacd.

--- ElmahCore.DemoCore3/Startup.cs
+++ ElmahCore.DemoCore3/Startup.cs
@@ -1,4 +1,5 @@
-﻿using ElmahCore.Mvc;
+using ElmahCore.Mvc;
+using ElmahCore.Sql;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -27,9 +28,20 @@ namespace ElmahCore.DemoCore3
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddElmah<XmlFileErrorLog>(options =>
+            //services.AddElmah<XmlFileErrorLog>(options =>
+            services.AddElmah<SqlErrorLog>(options =>
+                //services.AddElmah(options =>
             {
-                options.LogPath = "~/log"; // OR options.LogPath = "с:\errors";
+                //options.OnPermissionCheck = context => context.User.Identity.IsAuthenticated;
+                options.Path = @"elmah";
+                options.ConnectionString = "Server=.;Database=elmahtest;Trusted_Connection=True;";
+                options.LogPath = "~/errors.xml";
+                //options.SourcePaths = new []
+                //{
+                //    @"D:\tmp\ElmahCore.DemoCore3",
+                //    @"D:\tmp\ElmahCore.Mvc",
+                //    @"D:\tmp\ElmahCore"
+                //};
             });
             services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
             {
@@ -63,7 +75,7 @@ namespace ElmahCore.DemoCore3
             app.UseCookiePolicy();
 
             app.UseElmah();
-            app.UseElmahExceptionPage();
+
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(routes =>
