a335939f3bbd5bdd91f9ad7e0ff2b19f030ef035
Joshua Searles <jsearles@plex.com>
2023-12-23 02:46:55 -0500

    build: update actions

--- ci-build.ps1
+++ ci-build.ps1
@@ -8,8 +8,8 @@ dotnet clean -c Release
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
 
 # Building for a specific framework.
-dotnet build .\ElmahCore -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
-dotnet build .\ElmahCore.Mvc -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
-dotnet build .\ElmahCore.MySql -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
-dotnet build .\ElmahCore.MsSql -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
-dotnet build .\ElmahCore.Postgresql -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+dotnet build .\src\ElmahCore -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+dotnet build .\src\ElmahCore.Mvc -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+dotnet build .\src\ElmahCore.MySql -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+dotnet build .\src\ElmahCore.MsSql -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+dotnet build .\src\ElmahCore.Postgresql -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
