efc1b47455355ef10eef277c2ffa9a4a80638c78
Joshua Searles <jsearles@plex.com>
2023-12-23 03:04:51 -0500

    chore: fix tests

--- Tests/ElmahCore.Mvc.Tests/ErrorLogMiddlewareTests.cs
+++ Tests/ElmahCore.Mvc.Tests/ErrorLogMiddlewareTests.cs
@@ -2,9 +2,9 @@ using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
-using NSubstitute;
 using Xunit;
 
 namespace ElmahCore.Mvc.Tests;
@@ -19,7 +19,7 @@ public class ErrorLogMiddlewareTests
     public ErrorLogMiddlewareTests()
     {
         _requestDelegate = httpContext => Task.CompletedTask;
-        _options = Substitute.For<IOptions<ElmahOptions>>();
+        _options = Options.Create(new ElmahOptions());
         _elmahLogger = new ElmahExceptionLogger(_errorLog, _options, NullLogger<ElmahExceptionLogger>.Instance);
     }
 
@@ -28,6 +28,11 @@ public class ErrorLogMiddlewareTests
     {
         var _ = new ErrorLogMiddleware(_requestDelegate, _elmahLogger, _options);
         var httpContext = new DefaultHttpContext();
+
+        var services = new ServiceCollection();
+        services.AddSingleton(_elmahLogger);
+        httpContext.RequestServices = services.BuildServiceProvider();
+
         Func<Task> act = async () => await ElmahExtensions.RaiseError(httpContext, new Exception());
         act.Should().NotThrowAsync();
     }
